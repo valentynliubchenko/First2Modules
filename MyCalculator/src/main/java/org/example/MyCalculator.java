4a23e018ec239e55d958a97032ac825c4b45b42c
Lyubchenko Valentin <valentin.lyubchenko@extern.a1.at>
2023-03-06 16:27:04 +0200

    add lombok

--- MyCalculator/src/main/java/org/example/MyCalculator.java
+++ MyCalculator/src/main/java/org/example/MyCalculator.java
@@ -1,6 +1,13 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MyCalculator {
+	int a;
+	int b;
 	public int sum(int a , int b){
 		return a+b;
 	}
